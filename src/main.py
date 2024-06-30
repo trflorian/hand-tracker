6505f03906b97b7b373482ec7bcea210e09319ae
trflorian <florian.trautweiler@hispeed.ch>
2024-06-30 16:29:16 +0200

    create main function for code

--- src/main.py
+++ src/main.py
@@ -4,37 +4,41 @@ import mediapipe.python.solutions.hands as mp_hands
 import mediapipe.python.solutions.drawing_utils as mp_drawing
 import mediapipe.python.solutions.drawing_styles as mp_drawing_styles
 
-cap = cv2.VideoCapture(index=0)
+def run_hand_tracking_on_webcam():
+    cap = cv2.VideoCapture(index=0)
 
-with mp_hands.Hands(
-    model_complexity=0,
-    max_num_hands=2,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5,
-) as hands:
-    while cap.isOpened():
-        success, frame = cap.read()
-        if not success:
-            print("Ignoring empty camera frame...")
-            continue
+    with mp_hands.Hands(
+        model_complexity=0,
+        max_num_hands=2,
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5,
+    ) as hands:
+        while cap.isOpened():
+            success, frame = cap.read()
+            if not success:
+                print("Ignoring empty camera frame...")
+                continue
 
-        # Check the frame for hands
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        results = hands.process(frame_rgb)
+            # Check the frame for hands
+            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            results = hands.process(frame_rgb)
 
-        # Draw the hand annotations on the image
-        if results.multi_hand_landmarks:
-            for hand_landmarks in results.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(
-                    image=frame,
-                    landmark_list=hand_landmarks,
-                    connections=mp_hands.HAND_CONNECTIONS,
-                    landmark_drawing_spec=mp_drawing_styles.get_default_hand_landmarks_style(),
-                    connection_drawing_spec=mp_drawing_styles.get_default_hand_connections_style(),
-                )
+            # Draw the hand annotations on the image
+            if results.multi_hand_landmarks:
+                for hand_landmarks in results.multi_hand_landmarks:
+                    mp_drawing.draw_landmarks(
+                        image=frame,
+                        landmark_list=hand_landmarks,
+                        connections=mp_hands.HAND_CONNECTIONS,
+                        landmark_drawing_spec=mp_drawing_styles.get_default_hand_landmarks_style(),
+                        connection_drawing_spec=mp_drawing_styles.get_default_hand_connections_style(),
+                    )
 
-        cv2.imshow("Hand Tracking", cv2.flip(frame, 1))
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
+            cv2.imshow("Hand Tracking", cv2.flip(frame, 1))
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
 
-cap.release()
+    cap.release()
+
+if __name__ == "__main__":
+    run_hand_tracking_on_webcam()
