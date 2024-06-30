aa7d396225d536e82b6779a748ea1193075f0fef
trflorian <florian.trautweiler@hispeed.ch>
2024-06-30 15:46:03 +0200

    simplify and clarify frame loop code

--- src/main.py
+++ src/main.py
@@ -1,46 +1,40 @@
-import numpy as np
-
 import cv2
 
 import mediapipe.python.solutions.hands as mp_hands
 import mediapipe.python.solutions.drawing_utils as mp_drawing
 import mediapipe.python.solutions.drawing_styles as mp_drawing_styles
 
-# For webcam input:
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(index=0)
+
 with mp_hands.Hands(
-    model_complexity=0, min_detection_confidence=0.5, min_tracking_confidence=0.25
+    model_complexity=0,
+    max_num_hands=2,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5,
 ) as hands:
     while cap.isOpened():
-        success, image = cap.read()
+        success, frame = cap.read()
         if not success:
-            print("Ignoring empty camera frame.")
-            # If loading a video, use 'break' instead of 'continue'.
+            print("Ignoring empty camera frame...")
             continue
 
-        # To improve performance, optionally mark the image as not writeable to
-        # pass by reference.
-        image.flags.writeable = False
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        results = hands.process(image)
+        # Check the frame for hands
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = hands.process(frame_rgb)
 
-        # Draw the hand annotations on the image.
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        # Draw the hand annotations on the image
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
                 mp_drawing.draw_landmarks(
-                    image,
-                    hand_landmarks,
-                    mp_hands.HAND_CONNECTIONS,
-                    mp_drawing_styles.get_default_hand_landmarks_style(),
-                    mp_drawing_styles.get_default_hand_connections_style(),
+                    image=frame,
+                    landmark_list=hand_landmarks,
+                    connections=mp_hands.HAND_CONNECTIONS,
+                    landmark_drawing_spec=mp_drawing_styles.get_default_hand_landmarks_style(),
+                    connection_drawing_spec=mp_drawing_styles.get_default_hand_connections_style(),
                 )
 
-                landmark_coords = np.array([[l.x, l.y, l.z] for l in hand_landmarks.landmark])
-
-        # Flip the image horizontally for a selfie-view display.
-        cv2.imshow("MediaPipe Hands", cv2.flip(image, 1))
-        if cv2.waitKey(5) & 0xFF == 27:
+        cv2.imshow("Hand Tracking", cv2.flip(frame, 1))
+        if cv2.waitKey(1) & 0xFF == ord("q"):
             break
+
 cap.release()
